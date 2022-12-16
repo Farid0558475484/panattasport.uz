5727f073ac951535eb9d5b7c7b8f484f449135b5
Farid0558475484 <faridmh@code.edu.az>
2022-12-16 20:16:14 +0400

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,34 @@ lena.name = "Elena";
 
 const qwerty = "asdfgh";
  
+
+// sort array
+
+const sortArray =()=>{
+  let issorted = false;
+  newAz.addEventListener("click", () => {
+    if (!issorted) {
+      issorted = true;
+      az();
+    } else {
+      issorted = false;
+      za();
+    }
+  }
+  );
+
+}
+
+const az = () => {  
+  const sorted = [...products].sort((a, b) => {
+    return a.price - b.price;
+  });
+  renderProducts(sorted);
+}
+
+const za = () => {
+  const sorted = [...products].sort((a, b) => {
+    return b.price - a.price;
+  });
+  renderProducts(sorted);
+}
