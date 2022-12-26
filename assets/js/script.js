d7849a5261c33c295f6ee7f0bae1b4894a0e6496
Farid0558475484 <faridmh@code.edu.az>
2022-12-26 18:09:38 +0400

    login page start

--- assets/js/script.js
+++ assets/js/script.js
@@ -5980,3 +5980,31 @@ array.forEach(element => {
 
   
 });
+
+
+const map = document.getElementById('map');
+const country = document.querySelectorAll('.land');
+const toolTip = document.getElementById('tooltip');
+const message = toolTip.innerHTML;
+
+// grab the country name and display
+function showCountryName() {
+    let countryName = event.target.getAttribute('title');
+    toolTip.innerHTML = countryName;
+}
+
+// show the default text
+function hideCountryName() {
+  toolTip.innerHTML = message;
+}
+
+// set event listener on the map
+map.addEventListener('mouseover', function(event){
+  // if the mouse hovers over a country
+  if (event.target.classList.contains('land')) {
+    showCountryName();
+  }
+  else {
+    hideCountryName();
+  }
+});
